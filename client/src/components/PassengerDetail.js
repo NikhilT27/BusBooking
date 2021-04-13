8723ab31e5e31f3ca00ff9591bf739dedbed4ce3
NikhilT27 <nikhilthakare14@gmail.com>
2021-04-13 23:26:43 +0530

    add: successful payment component

--- client/src/components/PassengerDetail.js
+++ client/src/components/PassengerDetail.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import EachPassengerForm from "./EachPassengerForm";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
@@ -17,7 +17,10 @@ import {
 import ContactForm from "./ContactForm";
 import letter from "../images/letter.svg";
 import person from "../images/person.svg";
+import checked from "../images/checked.svg";
 import moment from "moment";
+import Loading from "./Loading";
+import LoadingPayment from "./LoadingPayment";
 
 export default function PassengerDetail({
   handleCloseClick,
@@ -38,13 +41,24 @@ export default function PassengerDetail({
   const history = useHistory();
 
   const [isSubmitForm, setIsSubmitForm] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  function SubmitForm() {
-    setIsSubmitForm(true);
+  useEffect(() => {
     if (seats.length === passengerData.length && contactData.length != 0) {
       console.log("Save Data to Database");
       bookTickets();
     }
+    return () => {
+      setIsSubmitForm(false);
+    };
+  }, [isSubmitForm == true]);
+
+  function SubmitForm() {
+    setIsSubmitForm(true);
+    // if (seats.length === passengerData.length && contactData.length != 0) {
+    //   console.log("Save Data to Database");
+    //   bookTickets();
+    // }
   }
 
   function UnSubmitForm() {
@@ -79,6 +93,7 @@ export default function PassengerDetail({
     );
 
     try {
+      setIsLoading(true);
       const responses = await axios.all(requests);
       if (responses && responses.length != 0) {
         dispatch(emptyAll());
@@ -92,11 +107,33 @@ export default function PassengerDetail({
             data: busData,
           },
         });
+        setIsLoading(false);
       }
       console.log(responses);
     } catch (err) {
       console.log(err.message);
     }
+  }
+
+  if (isLoading) {
+    return (
+      <div className="passenger">
+        <div className="payment-detail">
+          <div className="payment-loading-half-circle"></div>
+          <div className="payment-box">
+            <img src={checked} className="payment-logo" />
+            <h3 className="payment-title">Payment Successful</h3>
+          </div>
+          <div className="payment-amount-box">
+            <span className="payment-amount-title">Amount Paid: </span>
+            <span className="payment-amount-value">Rs. {total} </span>
+          </div>
+          <div className="payment-pdf">
+            <LoadingPayment title={"Generating your PDF ticket"} />
+          </div>
+        </div>
+      </div>
+    );
   }
 
   return (
